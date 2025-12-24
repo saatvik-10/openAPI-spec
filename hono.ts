c4009697c73b37dd29a0d158cb46ce7fffbe5ce6
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-24 22:48:24 +0530

    hono zod/openapi

---
+++ hono.ts
@@ -0,0 +1,58 @@
+import { z, createRoute, OpenAPIHono } from '@hono/zod-openapi';
+
+const app = new OpenAPIHono();
+
+const signUpInput = z
+  .object({
+    username: z.string().min(3),
+    password: z.string().min(8),
+  })
+  .openapi('UserCredentials');
+
+const signUpOutut = z
+  .object({
+    message: z.string(),
+  })
+  .openapi('UserSignupMessage');
+
+const route = createRoute({
+  method: 'post',
+  path: '/signup',
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: signUpInput,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: signUpOutut,
+        },
+      },
+      description: 'Creating the user',
+    },
+  },
+});
+
+app.openapi(route, async (c) => {
+  let { username, password } = c.req.valid('json');
+
+  return c.json({
+    message: 'User signedup successfully',
+  });
+});
+
+app.doc('/', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'API',
+  },
+});
+
+export default app;
